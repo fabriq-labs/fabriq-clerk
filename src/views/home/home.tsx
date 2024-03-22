b4a058a2202dd96f3d1c2bdf90e0d65bc2797f47
DevMuthuKumar <muthu@metalimits.com>
2024-03-22 16:43:56 +0530

    Fixed home page

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,17 +1,32 @@
-// Result
-import React from "react";
-import { Protect } from "@clerk/nextjs";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { Protect, useAuth } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 import { Overview } from "@views/overview/overview";
 import OverviewPage from "@views/overview/overview_main";
 import Layout from "@components/layout";
 
 export default function HomePage() {
+  const { has } = useAuth();
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const canManage = has && has({ permission: "org:feature:protected" });
+
+  useEffect(() => {
+    if (canManage) {
+      router.push("/pipeline");
+    }
+    setLoading(false);
+  }, [canManage]);
+
+  if (loading) {
+    return null;
+  }
+
   return (
     <Layout>
-      <Protect permission="org:feature:protected">
-        <Overview />
-      </Protect>
       <Protect permission="org:demo:all">
         <Overview />
       </Protect>
